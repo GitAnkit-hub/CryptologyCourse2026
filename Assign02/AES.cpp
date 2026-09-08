a319a05826af71f6145dee4499111040be41214b
GitAnkit-hub <ankitag26598@gmail.com>
2026-09-08 11:13:08 +0530

    Master Key is generated(will be deterministic even after every compilation)

--- Assign02/AES.cpp
+++ Assign02/AES.cpp
@@ -7,12 +7,12 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+#include <random>
+#include <sodium.h> // cryptographically secure random number generator
+#include<iomanip>
 vector<unsigned char> Round_const = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36};
 // since hex data is of 1 Byte
 // unsigned char and uint8_t are guaranteed to be 1 byte size
-
-// Key Scheduling Concept
-// ki = shiftrows(ki-1) ^ RCi
 
 vector<unsigned char> Master_key;
 
@@ -25,20 +25,37 @@ void shiftRows(unsigned char *state[4][4])
 // generate the master key
 // implement the shiftrows function
 // generate all the round keys
-#include <random>
-#include <sodium.h> // cryptographically secure random number generator
+
 
 void generateMasterKey()
-{
+{   
+    if(sodium_init() < 0){
+        cerr << "Failed to initialize libsodium\n";
+        return;
+    }
 
     // Initialize the master key with random values
     Master_key.resize(16); // AES-128 uses a 16-byte key
-    randombytes_buf(Master_key.data(), Master_key.size());
+     // A fixed 32-byte dummy seed for assignment testing
+    const unsigned char assignment_seed[randombytes_SEEDBYTES] = {
+        0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+        0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10,
+        0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18,
+        0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20
+    };
+    randombytes_buf_deterministic(Master_key.data(), Master_key.size(), assignment_seed);
+    cout << "Master Key: ";
+    for (const auto &byte : Master_key)
+    {
+        cout << hex << setw(2) << setfill('0') << static_cast<int>(byte) << " ";
+    }
+    cout << endl;
 }
 
-cout << "Master Key: ";
-for (const auto &byte : Master_key)
-{
-    cout << hex << static_cast<int>(byte) << " ";
+
+
+int main(){
+    generateMasterKey();
+    return 0;
 }
-cout << endl;
+
